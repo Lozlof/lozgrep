d516625027cd9c058ca588d0409a3e96e0186511
lozlof <lozlofcyber@outlook.com>
2024-11-14 14:09:47 +0000

    check_for_duplicate_options

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 pub mod parse_and_build_arguments {
     use std::env;
     use std::process;
+    use std::collections::HashSet;
 
     pub fn build_running_configuration() {
         let collected_arguments: Vec<String> = env::args().skip(1).collect(); // Will collect passed arguments and put them into a vector. Does not collect the first passed argument, because it is not needed.
         let possible_options: [&str; 14] = ["--help", "-h", "--version", "-ver", "--verbose", "-v", "--query", "-q", "--path", "-p", "--simple-grep", "-sg", "--simple-find", "-sf"]; // These are all the valid options.
         verify_argument_length(&collected_arguments); // Checks if zero arguments are passed, checks if too many arguments are passed, error in either senario.
-        verify_options_are_valid(&collected_arguments, &possible_options); // Filters and collects all options (--, -) from the arguments. Compares the filtered options to possible_options to verify the given options. Creates errors if bad options are present.
+        verify_options_are_valid(&collected_arguments, &possible_options); // Filters and collects all options (--, -) from the arguments. Compares the filtered options to possible_options to verify the given options. Creates errors if bad options are present. Calls on a function to check for duplicate options, and creates an error if there are duplicate options.
     }
 
     fn verify_argument_length(borrow_collected_arguments: &Vec<String>) {
@@ -45,6 +46,22 @@ pub mod parse_and_build_arguments {
                 println!("Invalid syntax. Unknown options were passed: {}. Use \"--help\" or \"-h\" to see options and syntax.", &print_bad_options);
                 process::exit(1);
             }
+        }
+
+        if check_for_duplicate_options(&filtered_options) == true {
+            print!("There are repeated options");
+        }
+    }
+
+    fn check_for_duplicate_options<T: Eq + std::hash::Hash>(borrow_filtered_options: &[T]) -> bool { // Generics (<T>): Allows the function to operate on slices of any data type. Eq Trait: Ensures that the elements can be compared for equality. Hash Trait: Allows the elements to be hashed, which is necessary for inserting them into a HashSet.
+        if borrow_filtered_options.len() != borrow_filtered_options // if borrow_filtered_options.len() != borrow_filtered_options.iter()..collect::<HashSet<_>>().len() {}.
+        .iter() // Creates an iterator over references to the elements (&T).
+        .collect::<HashSet<_>>()// Transforms the iterator into a collection, in this case, a HashSet. Using ::<HashSet<_>> explicitly tells Rust to collect into a HashSet, and the underscore _ lets the compiler infer the specific type.
+        .len() {
+            return true; // There are repeated options.
+            
+        } else {
+            return false; // There are not repeated options.
         }
     }
 }
