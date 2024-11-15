2b4a5a25254be50fec3910c4d0712d3c1e40b079
lozlof <lozlofcyber@outlook.com>
2024-11-15 16:02:55 +0000

    Write help menu

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use lozgrep::parse_and_build_arguments::build_running_configuration;
 fn main() {
     let passed_options: lozgrep::parse_and_build_arguments::Options = build_running_configuration();
 
-    if passed_options.help {println!("Help: true")};
-    if passed_options.version {println!("Version: true")};
+    if passed_options.version == true { print_version() }
+    if passed_options.help == true { print_help() }
+    
+    
+    
+    
     if passed_options.verbose {println!("Verbose: true")};
     if passed_options.query {println!("Query: true")};
     if passed_options.path {println!("Path: true")};
@@ -14,13 +18,35 @@ fn main() {
     if passed_options.path_item != "null" {println!("Path item: {}", passed_options.path_item)};
 }
 
-/* Options:
-help: bool,
-version: bool,
-verbose: bool,
-query: bool,
-path: bool,
-simple_grep: bool,
-simple_find: bool,
-query_item: String,
-path_item: String, */
+fn print_version() {
+    println!("lozgrep version 0.0.2");
+}
+
+fn print_help() { // TODO: Make more descriptive.
+    println!("Options:");
+    println!("--help          -h       Prints the help menu.");
+    println!("--version       -ver     Prints the current version.");
+    println!("--verbose       -v       Prints output statements while the process is running.");
+    println!("--query         -q       The term you are searching for follows this option.");
+    println!("--path          -p       The path you are searching follows this option.");
+    println!("--simple-grep   -sg      Searches the contents of a file.");
+    println!("--simple-find   -sf      Searches for a file or directory name.");
+    println!("");
+    println!("Syntax rules:");
+    println!("The options can come in any order.");
+    println!("The long option (--) or short option (-) can be used interchangeably.");
+    println!("");
+    println!("Examples:");
+    println!("lozgrep -sg -p /home/user/file -q wordiamlookingfor");
+    println!("lozgrep --help -ver --query filename --simple-find -p /root");
+    println!("");
+    println!("Escape character rules:");
+    println!("The escape character is: /");
+    println!("The escape character can only be used on the value you want to query.");
+    println!("Escape character examples:");
+    println!("If you need to query for phrase that happens to also be an option");
+    println!("lozgrep -sg -q /--help -p /home/user/file");
+    println!("The escape character is needed in this example because without it lozgrep will read --help as an option and not an item to look for.");
+    println!("Therefore if you need to query for / you need to escape it, otherwise it will be stripped and the query will be empty.");
+    println!("lozgrep -sg -q // -p /home/user/file");
+}
